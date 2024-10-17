c9868e71d6153b159cac208565ae1351b658bcd5
Beatriz Azevedo <beatrizazv@gmail.com>
2024-10-16 21:26:56 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -18,10 +18,11 @@ exibirMensagemInicial();
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
-    exibirTextoNaTela('h1', 'Acertou!')
-    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`
+
     if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!')
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`
         exibirTextoNaTela('p', mensagemTentativas)
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
